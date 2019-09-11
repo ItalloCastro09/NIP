15cde02ea90d272edda3139a8855f8f1f458f0dc
Itallo Castro <castrocast2@gmail.com>
2019-09-11 09:24:04 -0300

    Sobre

---
+++ sobre.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style>
+		@import url('https://fonts.googleapis.com/css?family=Arvo&display=swap');
+
+body {
+  font-family:'Arvo', serif;
+  text-align: center;
+}
+  ul{
+text-align: center;
+    color: black;
+    font-size: 20px;
+    background-color: rgba(255, 255, 255, 0.6);
+    border-radius: 10%;
+    text-decoration: none;
+    display: inline-block;
+  }
+  li:hover{
+    color: blue;
+    transition: 0.5s
+  }
+  div{
+margin: auto;
+width: 350px;
+margin-top: 40px;
+padding: 40px;
+border:1px solid black;
+border-radius: 1em;
+border-collapse:collapse; 
+  }
+
+	</style>
+}
+</head>
+<body>
+	<div class="sla">
+	<ul>
+	<li>Itallo Castro/ItalloCastro09</li>
+	<li>Jammilly Pereira/Jammilly</li>
+	<li>Joan Lucas/JoanLucasL</li>
+	<li>luana Florencio/Luanaflorencio</li>
+	<li>Lucas Felinto/LucasFelinto</li>
+	<li>Maele Rodrigues/MaeleRodrigues</li>
+	<li>Welyson Santana/...</li></ul></div>
+	<div><h2>castrocast2@gmail.com</h2>
+	<h2>pjaanf@gmail.com</h2>
+	<h2>joanlucaslopes@gmail.com</h2>
+	<h2>lua.m.016@gmail.com</h2>
+	<h2>elucasfelinto@gmail.com</h2>
+	<h2>maele.arruda@gmail.com.</h2>
+	<h2>well...</h2></div>
+</body>
+</html>
