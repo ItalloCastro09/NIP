8a1c33594c7af3232a46e6972c52029f54d9b87e
LucasFelinto <elucasfelinto@gmail.com>
2019-09-13 13:07:33 -0300

    home page

---
+++ src/components/menu-top.php
@@ -0,0 +1,14 @@
+<header class="header">
+  <div class="header__logo-box">
+    <h1 class="heading-logo">tradebook</h1>
+  </div>
+  <nav class="header-menu">
+    <ul class="header-menu__list">
+      <a href=""><li class="header-menu__item">home</li></a> 
+      <a href=""><li class="header-menu__item">sobre</li></a>
+      <a href=""><li class="header-menu__item">login</li></a>
+      <a href=""><li class="header-menu__item">Cadastrar</li></a>
+      <div class="line"></div>
+    </ul>
+  </nav>
+</header>
---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+	<?php include "components/header.php" ?>
+	<body>
+		<?php include "components/menu-top.php" ?>
+		<section class="info">
+			<h1 class="heading-primary">O que é trade book?</h1>
+			<p class="paragraph">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quibusdam consequuntur, consectetur quidem delectus necessitatibus, adipisci voluptatibus nulla totam ipsam ex error maiores! Possimus et obcaecati eveniet? Saepe, reprehenderit porro.
+			</p>
+		</section>
+	</body>
+</html>
