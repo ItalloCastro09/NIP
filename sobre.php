9497dbb1213537efc97f967587135949e9b91687
LucasFelinto <elucasfelinto@gmail.com>
2019-09-17 10:22:52 -0300

    nova estrutura de pasta

---
+++ sobre.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+	<?php include "components/header.php" ?>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+	<link rel="stylesheet" href="css/sobre.css">
+
+<body>
+
+<?php include "components/menu-top.php" ?>
+
+	<div class="testimonial-section">
+		<div class="inner-width">
+
+		  <h1>Contribuidores</h1>
+
+		  <div class="testimonial-pics">
+		    	<img src="img/Itallo.jpg" alt="i1" class="active">
+				<img src="img/Jammilly.jpg" alt="i2" class="active">
+				<img src="img/Joan.jpg" alt="i3" class="active">
+				<img src="img/Luana.jpg" alt="i4" class="active">
+				<img src="img/Lucas.jpg" alt="i5" class="active">
+				<img src="img/Maele.jpg" alt="i6" class="active">
+				<img src="img/Welyson.jpg" alt="i7" class="active">
+			</div>
+		   </div>
+
+		   <div class="testimonial-contents">
+			<div class="testimonial active" id="i1">
+
+				<span class="description">Itallo Castro<br>GitHub: ItalloCastro09</span><br>
+				<span class="sp">castrocast2@gmail.com</span>
+
+		   </div>
+
+		   <div class="testimonial" id="i2">
+
+				<span class="description">Jammilly Pereira<br>GitHub: Jammilly</span><br>
+				<span class="sp">pjaanf@gmail.com</span>
+
+		   </div>
+
+		   <div class="testimonial" id="i3">
+
+				<span class="description">Joan Lucas<br>GitHub: JoanLucasL</span><br>
+				<span class="sp">joanlucaslopes@gmail.com</span>
+		   
+		   </div>
+
+		   <div class="testimonial" id="i4">
+
+				<span class="description">Luana Florencio<br>GitHub: Luanaflorencio</span><br>
+				<span class="sp">lua.m.016@gmail.com</span>
+
+		   </div>
+
+		   <div class="testimonial" id="i5">
+
+				<span class="description">Lucas Felinto<br>GitHub: LucasFelinto</span><br>
+				<span class="sp">elucasfelinto@gmail.com</span>
+
+		   </div>
+
+		   <div class="testimonial" id="i6">
+
+				<span class="description">Maele Rodrigues<br>GitHub: MaeleRodrigues</span><br>
+				<span class="sp">maele.arruda@gmail.com</span>
+
+		   </div>
+
+		   <div class="testimonial" id="i7">
+
+				<span class="description">Welyson Santana<br>GitHub: ...</span><br>
+				<span class="sp">well...</span>
+
+		   </div>
+
+		   </div>
+		</div>
+
+	<script src="js/sobre.js"></script>
+	<script src="js/scrollMenuEfect.js"></script>
+
+
+</body>
+</html>
