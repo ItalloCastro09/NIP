d87d8cef338bf5cfa868aa34e0baa9a03666d18c
luanaflorencio <lua.m.016@gmail.com>
2019-09-16 22:55:53 -0300

    linkamento dos elementos do menu

--- src/components/menu-top.php
+++ src/components/menu-top.php
@@ -4,15 +4,10 @@
   </div>
   <nav class="header-menu">
     <ul class="header-menu__list">
-<<<<<<< HEAD
-      <li class="header-menu__item"><a data-color="white" href="">Home</a></li> 
-      <li class="header-menu__item"><a data-color="white" href="">Sobre</a></li>
-=======
-      <li class="header-menu__item"><a data-color="white" href="index.php">Home</a></li> 
-      <li class="header-menu__item"><a data-color="white" href="sobre.php">Sobre</a></li>
->>>>>>> 6c01f2c38240dcff2b90dcceb4a139be5eaa4c1f
+      <li class="header-menu__item"><a data-color="white" href="../index.php">Home</a></li> 
+      <li class="header-menu__item"><a data-color="white" href="../sobre.php">Sobre</a></li>
       <li class="header-menu__item"><a data-color="white" href="">Login</a></li>
-      <li class="header-menu__item"><a data-color="white" href="">Cadastrar</a></li>
+      <li class="header-menu__item"><a data-color="white" href="../cadastro.php">Cadastrar</a></li>
       <div class="line"></div>
     </ul>
   </nav>
