efdf7f0b5ccffb74ca8d1240790ee27e6b3ed4fe
Itallo Henrique Castilho Castro <49693805+ItalloCastro09@users.noreply.github.com>
2019-09-18 07:44:50 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<body>
 		<?php include "components/menu-top.php" ?>
 		<section class="info">
-			<h1 class="heading-primary">O que é trade book?</h1>
+			<h1 class="heading-primary">O que é Trade Book?</h1>
 			<p class="paragraph">
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima quibusdam consequuntur, consectetur quidem delectus necessitatibus, adipisci voluptatibus nulla totam ipsam ex error maiores! Possimus et obcaecati eveniet? Saepe, reprehenderit porro.
 			</p><link href="https://fonts.googleapis.com/css?family=Lora:700&display=swap" rel="stylesheet">
