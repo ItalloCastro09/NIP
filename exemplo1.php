b7cff25bbb815a723a31ea96b064d23d0f3ffee2
Jammilly <pjaanf@gmail.com>
2019-09-17 23:06:26 -0300

    adicionando exemplos

---
+++ exemplo1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "components/header.php" ?>
+<body>
+<div class="book__description">
+<img class="exemplodelivros" src="http://4.bp.blogspot.com/-uO59jBa8yGg/WBqFrzbAYyI/AAAAAAAADN0/3hp-nItF3Ds9NNwCg7M9uq77bMkpYp0kwCK4B/s1600/2-1280x853.jpg" alt="">
+<div class="description">
+	<h1>Por Lugares Incríveis</h1><br><br>
+	<h2>Ano: 2015</h2><h2>Autor: Jennifer Niven</h2>
+</div>
+</div>
+</body>
+</html>
---
+++ exemplo2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "components/header.php" ?>
+<body>
+<div class="book__description">
+<img class="exemplodelivros" src="https://resenhasalacarte.com.br/wp-content/uploads/2018/06/pequeno-principe-livro-frases2.jpg" alt="">
+<div class="description">
+	<h1>O Pequeno Príncipe</h1><br><br>
+	<h2>Ano: 1993</h2><h2>Autor: Antoine de Saint-Exupéry</h2>
+</div>
+</div>
+</body>
+</html>
