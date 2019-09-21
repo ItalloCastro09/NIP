1a08784e2e29adbf336d5ffd10bec25ceafff429
luanaflorencio <lua.m.016@gmail.com>
2019-09-21 20:24:28 -0300

    correção do login

--- process/auth.php
+++ process/auth.php
@@ -2,15 +2,20 @@
 $email = $_POST['e-mail'];
 $pwd = $_POST['senha'];
 
-$usuarios = file('dados.csv');
+$usuarios = file('../csv/dados.csv');
 foreach($usuarios as $usuario) {
     $usuarioData = explode(',', $usuario);
+    // var_dump($usuarioData, $email, $pwd);
     if (trim($usuarioData[2]) == $email && trim($usuarioData[3]) == $pwd) {
     	header('location: index.php');
+    	exit();
+    	
     }
     else{
-    	header('location: cadastro.php');
+    	
     }
+ }
+header('location: cadastro.php');
         //login($usuarioData[0], $usuarioData[2]);
         //break;
 ?>
