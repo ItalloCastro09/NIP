5d9fc62ece98e77ae96a8ef69f7999c23baac71e
Joan Lucas <joanlucaslopes@gmail.com>
2019-09-25 00:23:48 -0300

    atualizado os exemplos1.php, exemplos2.php, exemplos3.php e adicionado descriplivros.css

--- exemplo2.php
+++ exemplo2.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
 <?php include "components/header.php" ?>
+<link rel="stylesheet" type="text/css" href="css/descriplivros.css">
 <body>
 
 <?php include "components/menu-top.php" ?>
