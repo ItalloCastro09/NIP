e40760b759a6721e128b12141a2730696e01fd52
LucasFelinto <elucasfelinto@gmail.com>
2019-09-30 17:57:27 -0300

    banco de dados

--- cadastro.php
+++ cadastro.php
@@ -6,48 +6,51 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
 	<link rel="stylesheet" type="text/css" href="css/cadastroelogin.css">
 
-
-<div class="container" id="container">
-	<div class="form-container sign-in-container">
-		<form action="process/auth.php" method="POST" class="formuser">
-			<h1>Entrar</h1>
-			<input type="email" name="e-mail" placeholder="Email">
-			<input type="password" name="senha" placeholder="Senha">
-			<a href="#">Esqueceu a senha?</a>
-			<button>Entrar</button>	
-		</form>
-	</div>
-
-	<div class="form-container sign-up-container">
-		<form action="process/armazenado.php" method="POST" class="formuser">
-			<h1>Cadastro</h1>
-			<input type="text" name="nome" placeholder="Nome">
-			<input type="number" name="telefone" placeholder="Telefone">
-			<input type="email" name="e-mail" placeholder="Email">
-			<input type="password" name="senha" placeholder="Senha">
-			<button>Cadastrar</button>
-		</form>
-	</div>
-
-		<div class="overlay-container">
-			<div class="overlay">
-				<div class="overlay-panel overlay-left">
-					<h1>Já Tem Uma Conta?</h1>
-					<p>
-						Clique abaixo para Entrar.
-					</p>
-					<button class="ghost" id="signIn">Entrar</button>
-				</div>
-				<div class="overlay-panel overlay-right">
-					<h1>Não Tem Uma Conta?</h1>
-					<p>
-						Clique abaixo para Cadastrar.
-					</p>
-					<button class="ghost" id="signUp">Cadastro</button>
-				</div>
+	<div class="display">
+		<div class="container" id="container">
+			<div class="form-container sign-in-container">
+				<form action="process/auth.php" method="POST" class="formuser">
+					<h1>Entrar</h1>
+					<input type="email" name="e-mail" placeholder="Email">
+					<input type="password" name="senha" placeholder="Senha">
+					<a href="#">Esqueceu a senha?</a>
+					<button>Entrar</button>	
+				</form>
 			</div>
-		</div>
-</div>
+
+			<div class="form-container sign-up-container">
+				<form action="process/armazenado.php" method="POST" class="formuser">
+					<h1>Cadastro</h1>
+					<input type="text" name="nome" placeholder="Nome">
+					<input type="number" name="telefone" placeholder="Telefone">
+					<input type="email" name="e-mail" placeholder="Email">
+					<input type="password" name="senha" placeholder="Senha">
+					<button>Cadastrar</button>
+				</form>
+			</div>
+
+				<div class="overlay-container">
+					<div class="overlay">
+						<div class="overlay-panel overlay-left">
+							<h1>Já Tem Uma Conta?</h1>
+							<p>
+								Clique abaixo para Entrar.
+							</p>
+							<button class="ghost" id="signIn">Entrar</button>
+						</div>
+						<div class="overlay-panel overlay-right">
+							<h1>Não Tem Uma Conta?</h1>
+							<p>
+								Clique abaixo para Cadastrar.
+							</p>
+							<button class="ghost" id="signUp">Cadastro</button>
+						</div>
+					</div>
+				</div>
+		</div> 
+	</div>
+	
+		<?php include "components/footer.php" ?>
 		<script src="js/scrollMenuEfect.js"></script>
 		<script src="js/cel.js"></script>
 </body>
