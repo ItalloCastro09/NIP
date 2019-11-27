6ce6ac606a33ae363889b9c1f02b8af8ad9e5f96
Joan Lucas <joanlucaslopes@gmail.com>
2019-11-27 02:36:14 -0200

    Update Perfil.php

--- views/perfil.php
+++ views/perfil.php
@@ -10,16 +10,16 @@
 		<div class="message"><?= $_GET["msg"] ?></div>
 	<?php endif ?>
 
-  <form style="margin-top: 140px" class="form" action="/controller/updatePerfil.php" enctype="multipart/form-data" method="POST">
+  <form class="perfil" action="/controller/updatePerfil.php" enctype="multipart/form-data" method="POST">
     <h1>Alterar informações</h1>
-    <label class="form__label"for="nome">Nome:</label>
-    <input class="form__input" type="text" value="<?=$_SESSION["id_usuario"]["NOME"]?>" id="nome" name="nome">
-    <label class="form__label"for="nome">Sobrenome:</label>
-    <input class="form__input " type="text" value="<?=$_SESSION["id_usuario"]["SOBRENOME"]?>" name="sobrenome">
-    <label class="form__label"for="nome">Email:</label>
-    <input class="form__input" type="text" value="<?=$_SESSION["id_usuario"]["EMAIL"]?>" name="email">
-    <label class="form__label"for="nome">Telefone:</label>
-    <input class="form__input" type="text" value="<?=$_SESSION["id_usuario"]["TELEFONE"]?>" name="telefone">
+    <label class="perfil__label"for="nome">Nome:</label>
+    <input class="perfil__input" type="text" value="<?=$_SESSION["id_usuario"]["NOME"]?>" id="nome" name="nome">
+    <label class="perfil__label"for="nome">Sobrenome:</label>
+    <input class="perfil__input " type="text" value="<?=$_SESSION["id_usuario"]["SOBRENOME"]?>" name="sobrenome">
+    <label class="perfil__label"for="nome">Email:</label>
+    <input class="perfil__input" type="text" value="<?=$_SESSION["id_usuario"]["EMAIL"]?>" name="email">
+    <label class="perfil__label"for="nome">Telefone:</label>
+    <input class="perfil__input" type="text" value="<?=$_SESSION["id_usuario"]["TELEFONE"]?>" name="telefone">
     <input type="hidden" name="usuarioId" value="<?=$_SESSION["id_usuario"]["ID"]?>">
     <input type="submit" value="Upload"/>
   </form>
